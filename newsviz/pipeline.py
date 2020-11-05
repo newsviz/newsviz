72ea78989d9c8f35be2fe8f3fbb301ee505a5a48
Sviatoslav <iggisv9t@gmail.com>
2020-11-05 17:59:23 +0300

    add autorship and GPLv3 note

--- newsviz/pipeline.py
+++ newsviz/pipeline.py
@@ -1,3 +1,20 @@
+# Copyright © 2020 Sviatoslav Kovalev. All rights reserved.
+
+#    This file is part of NewsViz Project.
+#
+#    NewsViz Project is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    NewsViz Project is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with NewsViz Project.  If not, see <https://www.gnu.org/licenses/>.
+
 import configparser
 import logging
 import os
@@ -7,6 +24,7 @@ import json
 import joblib
 import luigi
 import pandas as pd
+import numpy as np
 import topic_model
 from preprocessing_tools import clean_text
 from preprocessing_tools import lemmatize
