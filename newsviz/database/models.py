a41b29fceb5613a851b11ca447eab88f80965a4b
Ksenia Stroykova <k.a.stroykova@gmail.com>
2020-12-21 23:53:51 +0300

    #68 cleanup database files and scripts

--- newsviz/database/models.py
+++ newsviz/database/models.py
@@ -2,8 +2,8 @@
 В данном файле описывается структура БД проекта, в данном файле
 должно находится описание всех таблиц базы данных.
 """
+from sqlalchemy import TIMESTAMP, Column, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, TIMESTAMP
 from sqlalchemy.schema import FetchedValue
 
 Base = declarative_base()
@@ -14,10 +14,16 @@ class BaseModel(Base):
     Абстрактный класс, описывающий все поля общие для всех таблиц. От
     него будут наследоваться остальные классы наших таблиц
     """
+
     __abstract__ = True
 
     created_at = Column(TIMESTAMP, nullable=False, server_default=FetchedValue())
-    updated_at = Column(TIMESTAMP, nullable=False, server_default=FetchedValue(), server_onupdate=FetchedValue())
+    updated_at = Column(
+        TIMESTAMP,
+        nullable=False,
+        server_default=FetchedValue(),
+        server_onupdate=FetchedValue(),
+    )
 
     def __repr__(self):
         return "<{0.__class__.__name__}(id={0.id!r})>".format(self)
@@ -25,12 +31,15 @@ class BaseModel(Base):
 
 # Таблица News
 class News(BaseModel):
-    __tablename__ = 'news'
+    __tablename__ = "news"
     id = Column(String, primary_key=True)
     date = Column(String)
     topic = Column(String)
     text = Column(String)
 
     def __repr__(self):
-        return "<News(date='%s', topic='%s', text='%s')>" % (self.date, self.topic, self.text)
-
+        return "<News(date='%s', topic='%s', text='%s')>" % (
+            self.date,
+            self.topic,
+            self.text,
+        )
