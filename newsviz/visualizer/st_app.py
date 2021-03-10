ae2e337efd3615025cea4edd8bef98100c18e8b8
Andrey <andlukyane@gmail.com>
2021-03-10 17:00:38 +0300

    Fix pre-commit errors

--- newsviz/visualizer/st_app.py
+++ newsviz/visualizer/st_app.py
@@ -4,16 +4,15 @@ import configparser
 
 import pandas as pd
 import streamlit as st
-
 import utils
 
 
 @st.cache(allow_output_mutation=True)
 def get_data():
-    PATH_CONFIG = '../config/config.ini'
+    PATH_CONFIG = "../config/config.ini"
     config = configparser.ConfigParser()
     config.read(PATH_CONFIG)
-    data_path = config['visualizer']['data_path']
+    data_path = config["visualizer"]["data_path"]
     loaded_container = utils.load_data(data_path)
     s0 = list(loaded_container.keys())[0]
     r0 = list(loaded_container[s0].keys())[0]
@@ -33,30 +32,31 @@ def update_data(container, option, rubric, agg_level, top_words, topic):
 
 
 def mp(type_chart_, df_, topic_):
-    if type_chart_ == 'Ridge plot':
+    if type_chart_ == "Ridge plot":
         return utils.ridge_plot(df_, topic_)
-    elif type_chart_ == 'Bump chart':
+    elif type_chart_ == "Bump chart":
         return utils.bump_chart(df_, topic_)
 
 
-st.title('NewsViz Project')
+st.title("NewsViz Project")
 
 container, source0, rubric0, top_words = get_data()
 
-option = st.sidebar.selectbox('Источник', ([s for s in container.keys()]))
-type_chart = st.sidebar.selectbox('Тип Графика', ('Ridge plot', 'Bump chart'))
-rubric = st.sidebar.selectbox('Рубрика', ([s for s in container[source0].keys()]))
-topic = st.sidebar.multiselect('Темы', ([s for s in container[source0][rubric0][1]]), default=['topic_0', 'topic_1'])
-agg_level = st.sidebar.selectbox('Уровень арргерации',
-                                 (['month', 'hour', 'day', 'week', 'year']),
-                                 format_func=lambda x: x.capitalize(),
-                                 )
+option = st.sidebar.selectbox("Источник", ([s for s in container.keys()]))
+type_chart = st.sidebar.selectbox("Тип Графика", ("Ridge plot", "Bump chart"))
+rubric = st.sidebar.selectbox("Рубрика", ([s for s in container[source0].keys()]))
+topic = st.sidebar.multiselect("Темы", ([s for s in container[source0][rubric0][1]]), default=["topic_0", "topic_1"])
+agg_level = st.sidebar.selectbox(
+    "Уровень арргерации",
+    (["month", "hour", "day", "week", "year"]),
+    format_func=lambda x: x.capitalize(),
+)
 
 df, top_words_df = update_data(container, option, rubric, agg_level, top_words, topic)
 
 figure = mp(type_chart, df, topic)
 
 st.plotly_chart(figure)
-st.header('Топ слов по темам')
+st.header("Топ слов по темам")
 st.subheader(rubric)
 st.dataframe(top_words_df)
